9c872e028125f06559b98e8a2ed1790a77592662
Prem Jatol <140836410+SmartlyPrem@users.noreply.github.com>
2024-02-28 22:21:45 +0530

    Update MainApp.jsx

--- src/Components/MainApp.jsx
+++ src/Components/MainApp.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MainApp = () => {
     return (
-        <div className='text-4xl h-[400px] font-semibold capitalize flex justify-center items-center'>
+        <div className='text-4xl h-[400px] text-center font-semibold capitalize flex justify-center items-center'>
             Welcome to our quiz game
         </div>
     );
